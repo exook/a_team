021578053ca216769738d40b2e819dcd45cb3e49
ShiQiu1 <shi.qiu.773@gmail.com>
2017-10-31 17:30:46 +0100

    update readData

--- code/readDataTest.cpp
+++ code/readDataTest.cpp
@@ -10,28 +10,38 @@ void readData(string filePath) {
 	// open input file
 	//ifstream file(filePath);
 	ifstream file("smhi-opendata_Lund_test.csv");
-	while (file) {
-		string helpstring;
-		while (getline(file, helpstring)) {
-			if(!helpstring.empty()) {
-		
+	if (file) {
+		cout << "Successfully reading data file" <<endl;
+	}
+	else {
+		cout << "Unable to open data file. Check the directory. " << endl;
+		return;
+	}
+
+	string helpstring;
+	while (getline(file, helpstring)) {
+		// skip empty lines
+		if(!helpstring.empty()) {		
 			istringstream hhelpstring(helpstring);
-			vector <string> record;
+			vector <string> record_of_row;
 		
 			while (hhelpstring) {
 				string data_of_row;
 				// break a row into different i
-				if (!getline(hhelpstring, data_of_row, ';')) break;
+				if (!getline(hhelpstring, data_of_row_separated_by_semicolom, ';')) break;
+				
 				record_of_row.push_back(data_of_row);
 			}
 			data.push_back(record_of_row);
-			}
 		}
 	}
+	
 	if (!file.eof()) {
 		cerr<< "Error reading the file!\n";
 	}
+	
 	cout<<"data size = "<<data.size()<<endl;
+	cout<<"data size = "<<data[1].size()<<endl;
 	for (int i=0; i<data.size(); i++){
 		cout<<data[i][0]<<endl;
 	}
