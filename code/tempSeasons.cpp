5c3d6a05f0ac774a5dd15b0b206a2799ced63c9a
Jessamy Mol <jessamymol@MacBook-Pro-van-Jessamy.local>
2017-11-01 23:06:31 +0100

    Started working on calcAverageTemp function

--- code/tempSeasons.cpp
+++ code/tempSeasons.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 #include "tempTrender.h"
 
+////calculate average temperature on each day
+//void calcAverageTemp(const vector <vector <string> > &data, vector <vector <int> > &averageTemp){
+//    string year;
+//    string month;
+//    string day;
+//    
+//    int tempSum;
+//    int counterTimes;
+//    
+//    
+//    //loop through elements in the vector and print them
+//    for (int i=0; i < int(data.size()); i++)
+//    {
+//        year = data.at(i).at(0);
+//        month = data.at(i).at(1);
+//        day = data.at(i).at(2);
+//        
+//        for (int j = 0; j < int(data.at(i).size()); j++)
+//        {
+//            //cout << vec.at(i).at(j) << " ";
+//        }
+//        //cout << endl;
+//    }
+//}
+
+//creates day a season starts to year histograms for all seasons
 void tempTrender::startDaySeasons(){
     cout << endl;
     cout << "Calculating on which day the seasons start each year" << endl;
     
     vector <vector <string> > dataSeasons;
+    //vector <vector <int> > averageTempDay;
+    
     readData("smhi-opendata_Lund.csv", dataSeasons);
     
-    print(dataSeasons);
+    print(dataSeasons, 10);
 }
 
 
