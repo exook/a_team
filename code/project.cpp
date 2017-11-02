92481d1347b42a702542f7443ae08543857ea208
emelieolsson <emelielinneaolsson@gmail.com>
2017-11-02 11:49:41 +0100

    Added tempDay to temptrender

--- code/project.cpp
+++ code/project.cpp
@@ -2,12 +2,12 @@
 #include <string>
 
 void project() {
-	string pathToFile = "../datasets/"; //Put the path to your data file here
+	string pathToFile = "/Users/emelieolsson/Documents/MasterPartikel/a_team/datasets/"; //Put the path to your data file here
 	tempTrender t(pathToFile); //Instantiate your analysis object
     
-    t.startDaySeasons();
+    //t.startDaySeasons();
 	
-	//t.tempOnDay(8, 23); //Call some functions that you've implemented
+	t.tempOnDay(8, 23); //Call some functions that you've implemented
 	//t.tempOnDay(235);
 	//t.tempPerDay();
 	//t.hotCold();
