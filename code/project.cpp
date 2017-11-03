48a501b345d2e779d9e45742b178af9b28524d85
Alexander Ekman <alexander.ekman95@gmail.com>
2017-11-03 15:27:14 +0100

    created the temoerature of day func. Added TempDat to temptrender. Updated cahngelog. Created temodaynumber. Adjusted other files to make tempdaynumber to run. Changes to the less important files. Conflicts in binary files

--- code/project.cpp
+++ code/project.cpp
@@ -2,13 +2,13 @@
 #include <string>
 
 void project() {
-	string pathToFile = "../datasets/"; //Put the path to your data file here
+	string pathToFile = "/Users/emelieolsson/Documents/MasterPartikel/a_team/datasets/"; //Put the path to your data file here
 	tempTrender t(pathToFile); //Instantiate your analysis object
     
-    t.startDaySeasons();
+    //t.startDaySeasons();
 	
 	//t.tempOnDay(8, 23); //Call some functions that you've implemented
-	//t.tempOnDay(235);
+	//t.tempOnDayNumber(306);
 	//t.tempPerDay();
 	//t.hotCold();
 	//t.tempPerYear(2050);
