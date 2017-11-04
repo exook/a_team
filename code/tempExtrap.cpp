9b3c8fc464b97a0e9edcc43d7d74071f1509910c
Alexander Ekman <alexander.ekman95@gmail.com>
2017-11-04 22:17:37 +0100

    tempExtrap now reads the data

--- code/tempExtrap.cpp
+++ code/tempExtrap.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "tempTrender.h"
+#include <vector>
 // ROOT library obejcts
 #include <TF1.h> // 1d function class
 #include <TH1.h> // 1d histogram classes
@@ -8,7 +9,16 @@
 #include <TCanvas.h> // canvas object
 #include <fstream>
 #include <TRandom3.h>
+
+using namespace std;
+
 void tempTrender::tempEx(){
+    vector <vector <string> > data;
+    readData("smhi-opendata_Lund.csv", data);
+	//for(size_t i = 0; i < dataOnDay.size(); ++i) cout<<dataOnDay.at(i)<<endl;
+	cout<<data.at(1).at(6)<<endl;
+
+/*
 	gRandom=new TRandom3();
         // create a histogram 
         TH1D* hist = new TH1D("data", ";x;N", 20, 0.0, 100.0);
@@ -18,5 +28,9 @@ void tempTrender::tempEx(){
                 hist->Fill(gRandom->Gaus(65.0, 5.0));
 
         TCanvas * c1= new TCanvas("c1", "random",5,5,800,600);
-        //hist->Draw();
+        hist->Draw();
+
+	    vector <vector <string> > dataOnDay;
+    readData("smhi-opendata_Lund.csv", dataOnDay);
+*/
 }
