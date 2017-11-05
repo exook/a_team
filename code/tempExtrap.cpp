0639b5ef5326b5f1ced36ae0e8e923b0482c714c
Alexander Ekman <alexander.ekman95@gmail.com>
2017-11-05 14:00:56 +0100

    readDataOld in tempExtrap now uses references inted of arguments. And thedata vector has float data types instead of strings

--- code/tempExtrap.cpp
+++ code/tempExtrap.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-vector<vector<string>> readDataOld(){	
+void readDataOld(vector<vector<float>>* dataVectorPointer){	
 	ifstream file("/home/alexander/Desktop/a_team/datasets/uppsala_tm_1722-2013.dat");
 	//check if opened correctly
     if (!file) {
@@ -23,40 +23,66 @@ vector<vector<string>> readDataOld(){
     }
 
     string value;
-    vector <vector <string>> dataVector;
-    string line;
 
+    string line;
     while (getline(file,line)){
         istringstream helpstring(line);
-        vector <string> row;
+        vector <float> row;
         while(getline(helpstring,value,' ')){
             if (value.find_first_not_of(' ') != std::string::npos){
-                row.push_back(value);
+                float number=strtof(value.c_str(),NULL);
+                row.push_back(number);
             }
         
         }
-        dataVector.push_back(row);
+        dataVectorPointer->push_back(row);
     }
     file.close();
-    return dataVector;
 }
 
-
+int isLeapYear(int year){
+    if((year % 4 == 0 && year % 100 != 0) || ( year % 400 == 0)){
+        return 1;
+    }
+    else{
+        cout<<"regular year"<<endl;
+        return 0;
+    }
+}
+/*
+void totalAverage(vector<vector<string>>* dataVectorPointer){
+    for(size_t i = 0; i < dataVectorPointer->size(); ++i){
+        //cout<<dataVectorPointer->at(i).at(4)<<endl;
+        yearlySum=0;
+            for(int j;j<365;j++){
+                yearlySum=yearlySum+dataVectorPointer->at(i).at(4);
+            }
+        yearlySum/365;
+    }
+    
+}
+*/
 void tempTrender::tempEx(){
-	vector <vector <string> > data;
-	data=readDataOld();
-    //gRandom=new TRandom3();
+
+    vector <vector <float>> dataVector;
+    vector <vector <float>> *dataVectorPointer=&dataVector;
+    
+    readDataOld(dataVectorPointer);
+
+	//vector <vector <string> > data;
+	//data=readDataOld();
     TH1D* hist = new TH1D("data", ";x;N", 100, -50, 50);
 
-    for(size_t i = 0; i < data.size(); ++i){
-        //cout<<data.at(i).at(4);
-        //hist->Fill(gRandom->Gaus(65.0, 5.0));
-        float value;
-        //value=string_to_float(data.at(i).at(4));
-        hist->Fill(strtof(data.at(i).at(4).c_str(),NULL));
+    for(size_t i = 0; i < dataVector.size(); ++i){
+        hist->Fill(dataVector.at(i).at(4));
+        //hist->Fill(strtof(dataVector.at(i).at(4).c_str(),NULL));
+        
     }
 
     TCanvas * c1= new TCanvas("c1", "random",5,5,800,600);
     hist->Draw();
+
+    //totalAverage(dataVectorPointer);
+    //cout<<totalAverage<<endl;
 
 }
