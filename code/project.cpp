930869cbe7e30a07561ce8493d51597282a5087a
ShiQiu1 <sh4722qi-s@student.lu.se>
2017-11-16 16:58:42 +0100

    improve the report and slightly modify the code

--- code/project.cpp
+++ code/project.cpp
@@ -8,10 +8,10 @@ void project() {
     
     tempTrender t(pathToFile); //Instantiate your analysis object
     
-   // t.startDaySeasons();
-   // temperature = t.tempEx(year);
-    //cout << endl << "The temperature in "<<year<<" will be: " << temperature << " degrees" << endl;
-	//t.tempOnDay(7, 19);
-	//t.tempOnDayNumber(198);
+    t.startDaySeasons();
+    temperature = t.tempEx(year);
+    cout << endl << "The temperature in "<<year<<" will be: " << temperature << " degrees" << endl;
+	t.tempOnDay(7, 19);
+	t.tempOnDayNumber(198);
 	t.hotCold("uppsala_tm_1722-2013.dat");
 }
